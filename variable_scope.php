ad0f5443990576987f90b7232e1965ed44013ad4
safayet-hossian <shehab.shafayet@gmail.com>
2024-12-30 00:22:26 +0600

    sum,sub,mul,div

--- variable_scope.php
+++ variable_scope.php
@@ -1,12 +1,14 @@
 <?php
  $x=2; 
  $y=3;
- $z=$x+$y;
+ $bappy=$x+$y;
  $sub=$y-$x;
+ $rony=$bappy * $sub;
+ $taka=$rony / $bappy;
  
 function sum(){
-   global $x,$y,$z;
-   echo "Sum of the function is $z<br>" ;
+   global $x,$y,$bappy;
+   echo "Sum of the function is $bappy<br>" ;
 }
 sum();
 
@@ -17,4 +19,17 @@ function sub(){
    echo "subtraction of the function is $sub <br>";
 }
 sub();
+
+function mal(){
+    global $rony;
+    echo "Rony bro boleche $rony ta mal ante<br>";
+
+}
+mal();
+
+function dorbesh(){
+    global $taka;
+    ECHO "dorbesher share $taka % shob project ei";
+}
+dorbesh();
 ?>
