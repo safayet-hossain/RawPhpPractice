84ebaf2b0c8f3242d02404534ddbe660167c51d3
safayet-hossian <shehab.shafayet@gmail.com>
2024-12-29 23:18:47 +0600

    syntax

---
+++ color.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$color = "red";
+$temp=$color;
+$shehab=$temp;
+echo "My car is " . $shehab . "<br>";
+echo "My house is " . $color . "<br>";
+echo "My boat is " . $temp . "<br>";
+?>
+
+</body>
+</html>
