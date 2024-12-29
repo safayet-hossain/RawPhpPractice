92da9dfe0c230b1065bd146079835065b5cf52e7
safayet-hossian <shehab.shafayet@gmail.com>
2024-12-29 23:13:13 +0600

    teaching to my little brother

---
+++ welcome.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+ 
+<?php
+echo "Welcome bro!" ;
+?>
+<br>
+<?php
+$name='shehab';
+echo "My name is $name  "
+?>
+<br>
+<?php
+$phpversion=phpversion();
+echo "My php version is $phpversion";
+?>
+</body>
+</html>
