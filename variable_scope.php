e4575410c13e7915d55c4473761d581846a8d3a9
safayet-hossian <shehab.shafayet@gmail.com>
2024-12-30 00:09:36 +0600

    sub

--- variable_scope.php
+++ variable_scope.php
@@ -2,9 +2,19 @@
  $x=2; 
  $y=3;
  $z=$x+$y;
+ $sub=$y-$x;
+ 
 function sum(){
    global $x,$y,$z;
-   echo "Sum of the function is $z";
+   echo "Sum of the function is $z<br>" ;
 }
 sum();
+
+
+function sub(){
+    
+    global $sub;
+   echo "subtraction of the function is $sub <br>";
+}
+sub();
 ?>
